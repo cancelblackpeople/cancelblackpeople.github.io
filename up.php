4c9d53cc536ffc5e39d4cf01646866c3e3341b7d
cancelblackpeople <85858640+cancelblackpeople@users.noreply.github.com>
2021-06-13 21:49:13 -0700

    Update up.php

--- up.php
+++ up.php
@@ -1,7 +1,7 @@
 <?php
 $secret_key = "test"; //Set this as your secret key, to prevent others uploading to your server.
 $sharexdir = "images/"; //This is your file dir, also the link..
-$domain_url = 'http://ss.s2ns.xyz/'; //Add an S at the end of HTTP if you have a SSL certificate.
+$domain_url = 'http://ss.s1ns.xyz/'; //Add an S at the end of HTTP if you have a SSL certificate.
 $lengthofstring = 5; //Length of the file name
 
 function RandomString($length) {
